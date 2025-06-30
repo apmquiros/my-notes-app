8545016a276283df8b3c2fea97fba42dff18056d
apmquiros <adrianepatrickquiros@gmail.com>
2025-06-30 10:58:59 +0800

    Restrict CORS to my GitHub Pages and localhost

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ require ('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://apmquiros.github.io",
+        "http://localhost:4200"
+    ]
+}));
 app.use(express.json());
 
 app.use('/api/notes', notesRouter);
